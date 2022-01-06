e2b8729ae3639167d06b2341e60c887581ac63fe
alessandrojuza <mascia.alessandro89@gmail.com>
2022-01-06 17:08:16 +0100

    Started building New Game modal

--- src/components/NewGame.js
+++ src/components/NewGame.js
@@ -1,20 +1,29 @@
 import React from "react";
 import "../styles/NewGame.scss";
+import logoblue from "../img/logo-blue.png";
 
 const NewGame = () => {
   return (
     <div className="new-game">
-      <h3>TESTO</h3>
-      <p>TESTO TESTO</p>
-      <p>TESTO TESTO</p>
-      <p>TESTO TESTO</p>
-      <p>TESTO TESTO</p>
-      <p>TESTO TESTO</p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate
-        quasi nisi id ut, voluptatibus magnam itaque a ea voluptatem accusamus.
-        Tempore accusantium recusandae doloribus perspiciatis?
-      </p>
+      <img src={logoblue} alt="logo" className="logo-blue" />
+      <h3>START A NEW GAME</h3>
+      <div className="game-type-wrapper">
+        <p>Game type:</p>
+        <div className="game-type-numbers">
+          <h4>101</h4>
+          <h4>301</h4>
+          <h4>501</h4>
+        </div>
+      </div>
+      <div className="player-name-wrapper">
+        <p>Player 1 Name:</p>
+        <input type="text" />
+      </div>
+      <div className="player-name-wrapper">
+        <p>Player 2 Name:</p>
+        <input type="text" />
+      </div>
+      <button>LET'S GET STARTED!</button>
     </div>
   );
 };
