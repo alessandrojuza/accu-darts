66e0b4541a7e9ff2c3a60e253f01abe3ec43bcf4
alessandrojuza <mascia.alessandro89@gmail.com>
2022-01-10 15:57:50 +0100

    Added turn functionality

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -18,30 +18,62 @@ const GameBoard = ({
 
   const [player1ScoreHistory, setPlayer1ScoreHistory] = useState([]);
   const [player2ScoreHistory, setPlayer2ScoreHistory] = useState([]);
+  const [player1Inactive, setPlayer1Inactive] = useState("");
+  const [player2Inactive, setPlayer2Inactive] = useState("inactive");
+  const [input1Hidden, setInput1Hidden] = useState("");
+  const [input2Hidden, setInput2Hidden] = useState("hidden");
+
+  const [score180Player1, setScore180Player1] = useState([]);
+  const [score160Player1, setScore160Player1] = useState([]);
+  const [score140Player1, setScore140Player1] = useState([]);
+  const [score120Player1, setScore120Player1] = useState([]);
+  const [score100Player1, setScore100Player1] = useState([]);
+
+  const [score180Player2, setScore180Player2] = useState([]);
+  const [score160Player2, setScore160Player2] = useState([]);
+  const [score140Player2, setScore140Player2] = useState([]);
+  const [score120Player2, setScore120Player2] = useState([]);
+  const [score100Player2, setScore100Player2] = useState([]);
 
   const addScorePlayer1 = () => {
     if (isNaN(player1ScoreInput)) setPlayer1Score(player1Score);
     else {
-      setPlayer1Score(player1Score - player1ScoreInput);
-      setPlayer1ScoreHistory([player1ScoreInput, ...player1ScoreHistory]);
+      if (player1ScoreInput <= player1Score) {
+        setPlayer1Score(player1Score - player1ScoreInput);
+        setPlayer1ScoreHistory([player1ScoreInput, ...player1ScoreHistory]);
+        setPlayer1Inactive("inactive");
+        setPlayer2Inactive("");
+        setInput1Hidden("hidden");
+        setInput2Hidden("");
+      } else {
+        setPlayer1Score(player1ScoreInput - player1Score);
+      }
     }
   };
 
   const addScorePlayer2 = () => {
     if (isNaN(player2ScoreInput)) setPlayer1Score(player2Score);
     else {
-      setPlayer2Score(player2Score - player2ScoreInput);
-      setPlayer2ScoreHistory([player2ScoreInput, ...player2ScoreHistory]);
+      if (player2ScoreInput <= player2Score) {
+        setPlayer2Score(player2Score - player2ScoreInput);
+        setPlayer2ScoreHistory([player2ScoreInput, ...player2ScoreHistory]);
+        setPlayer2Inactive("inactive");
+        setPlayer1Inactive("");
+        setInput2Hidden("hidden");
+        setInput1Hidden("");
+      } else {
+        setPlayer2Score(player2ScoreInput - player2Score);
+      }
     }
   };
 
   return (
     <div className={`game-board ${newGameIsVisible}`}>
-      <div className="div1">
+      <div className={`div1 ${player1Inactive}`}>
         <h1 className="points">{player1Score}</h1>
         <h4 className="player-name">{player1}</h4>
       </div>
-      <div className="div2">
+      <div className={`div2 ${player2Inactive}`}>
         <h1 className="points">{player2Score}</h1>
         <h4 className="player-name">{player2}</h4>
       </div>
@@ -52,9 +84,10 @@ const GameBoard = ({
           onChange={(e) => {
             setPlayer1ScoreInput(e.target.value);
           }}
+          className={input1Hidden}
         />
         <ArrowCircleRightRoundedIcon
-          className="btn-arrow"
+          className={`btn-arrow ${input1Hidden}`}
           onClick={addScorePlayer1}
         />
       </div>
@@ -65,29 +98,45 @@ const GameBoard = ({
           onChange={(e) => {
             setPlayer2ScoreInput(e.target.value);
           }}
+          className={input2Hidden}
         />
         <ArrowCircleRightRoundedIcon
-          className="btn-arrow"
+          className={`btn-arrow ${input2Hidden}`}
           onClick={addScorePlayer2}
         />
       </div>
-      <div className="div5">
-        <p>CIAO</p>
+      <div className={`div5 ${player1Inactive}`}>
+        <h4>Game Average:</h4>
+        <p></p>
+        <h4>Amount:</h4>
+        <p>{`180: ${score180Player1}`}</p>
+        <p>{`160-179: ${score160Player1}`}</p>
+        <p>{`140-159: ${score140Player1}`}</p>
+        <p>{`120-139: ${score120Player1}`}</p>
+        <p>{`100-119: ${score100Player1}`}</p>
       </div>
-      <div className="div6">
-        <p>Score History:</p>
+      <div className={`div6 ${player1Inactive}`}>
+        <h4>Score History:</h4>
+
         {player1ScoreHistory.map((e) => {
           return <h4>{e}</h4>;
         })}
       </div>
-      <div className="div7">
-        <p>Score History:</p>
+      <div className={`div7 ${player2Inactive}`}>
+        <h4>Score History:</h4>
         {player2ScoreHistory.map((e) => {
           return <h4>{e}</h4>;
         })}
       </div>
-      <div className="div8">
-        <p>CIAO</p>
+      <div className={`div8 ${player2Inactive}`}>
+        <h4>Game Average:</h4>
+        <p></p>
+        <h4>Amount:</h4>
+        <p>{`180: ${score180Player2}`}</p>
+        <p>{`160-179: ${score160Player2}`}</p>
+        <p>{`140-159: ${score140Player2}`}</p>
+        <p>{`120-139: ${score120Player2}`}</p>
+        <p>{`100-119: ${score100Player2}`}</p>
       </div>
     </div>
   );
