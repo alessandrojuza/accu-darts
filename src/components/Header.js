0e3fe8b5c51745534b5e950d024648f69c6905f1
alessandrojuza <mascia.alessandro89@gmail.com>
2022-01-13 10:59:59 +0100

    Completed Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,8 +11,13 @@ const Header = () => {
       <FadeIn transitionDuration="1000" delay="1300">
         <div className="header-sub">
           <h3 onClick={refreshPage}>NEW GAME</h3>
-          <h3>ABOUT</h3>
-          <h3>CONTACT</h3>
+          <a
+            href="https://github.com/alessandrojuza/accu-darts/blob/master/README.md"
+            target="_blank"
+          >
+            ABOUT
+          </a>
+          <a href="mailto:mascia.alessandro89@gmail.com">CONTACT</a>
         </div>
       </FadeIn>
     </div>
